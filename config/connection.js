b12618c4cf942e7be3ceb225844178beaa52b77f
ashishcheenikal <ashishcheenikal@gmail.com>
2022-08-21 14:06:48 +0530

    Added MongoDB  Atlas

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const env = require('dotenv').config()
 
 let config = {
-    mongoUri : 'mongodb://localhost:27017/VaaHan',
+    mongoUri : `mongodb+srv://AshishCheenikal:${process.env.ATLAS_PASSWORD}@cluster0.0p1xuis.mongodb.net/VaaHan`,
     connect: () => {
         mongoose.connect(config.mongoUri).then((res) => {
             console.log("mongodb connected")
